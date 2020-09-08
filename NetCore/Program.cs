91bd1f3dac8930e09bbf70a7dc272d7beb47d749
hnnna <hnnna@DESKTOP-R93P64C>
2020-09-08 10:55:33 +0700

    Add project files.

---
+++ NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test net core va github");
+        }
+    }
+}
